123f300d1eff47589b0936c4d72b008988d4ddf6
TheCodingCat <prteza@gmail.com>
2020-01-31 23:58:34 -0800

    Reformat
    Removed extra {} to make code cleaner

--- inject.js
+++ inject.js
@@ -51,69 +51,53 @@ setInterval(function(){
 		
 	//Perform acts of wizardry
 	if(cliptoggle){
-		if(38 in keysDown){//up
+		if(38 in keysDown)//up
 			player.pos.y -= 16 //Manyland uses a coordinate system originating from the topleft so this is up
-		}
-		if(40 in keysDown){//down
+		if(40 in keysDown)//down
 			player.pos.y += 16
-		}
-		if(37 in keysDown){//left
+		if(37 in keysDown)//left
 			player.pos.x -= 16
-		}
-		if(39 in keysDown){//right
+		if(39 in keysDown)//right
 			player.pos.x += 16
-		}
-		
 	}
 	
 	if(flytoggle){
-		if(38 in keysDown){//up
+		if(38 in keysDown)//up
 			player.vel.y -= 1 
-		}
 		if(40 in keysDown){//down
 			player.vel.y = 0.001 //Has to work this way to prevent fall death
 			player.pos.y += 1
 		}
-		if(37 in keysDown){//left
+		if(37 in keysDown)//left
 			player.vel.x -= 1
-		}
-		if(39 in keysDown){//right
+		if(39 in keysDown)//right
 			player.vel.x += 1
-		}
 		
 	}
 	
 	if(freecamtoggle){
-		if(38 in keysDown){//up
+		if(38 in keysDown)//up
 			ig.game.camera.offset.y -= 5
-		}
-		if(40 in keysDown){//down
+		if(40 in keysDown)//down
 			ig.game.camera.offset.y += 5
-		}
-		if(37 in keysDown){//left
+		if(37 in keysDown)//left
 			ig.game.camera.offset.x -= 5
-		}
-		if(39 in keysDown){//right
+		if(39 in keysDown)//right
 			ig.game.camera.offset.x += 5
-		}
 	}
 	
 	if(speedtoggle){
-		if(38 in keysDown){//up
+		if(38 in keysDown)//up
 			player.vel.y -= 0.5
-		}
-		if(40 in keysDown){//down
+		if(40 in keysDown)//down
 			player.vel.y += 0.5
-		}
-		if(37 in keysDown){//left
+		if(37 in keysDown)//left
 			player.vel.x -= 0.5
-		}
-		if(39 in keysDown){//right
+		if(39 in keysDown)//right
 			player.vel.x += 0.5
-		}
 	}
 	
-	if(gravitytoggle) ig.game.gravity = newgravitye
+	if(gravitytoggle) ig.game.gravity = newgravity
 	else gravitytoggle = 800 //default gravity
 	
 	
